d2c568f26d3f8152354b094a9abe8382c42eecbc
Kashish <kashish.biserwal@protonmail.com>
2025-07-12 16:48:14 +0530

    93

--- controller/admin.controller.ts
+++ controller/admin.controller.ts
@@ -1165,7 +1165,7 @@ const uploadSheet = async (req: ExtendedRequest, res: Response, next: NextFuncti
                                 oversLeft: parseInt(row.oversLeft),
                                 price: parseInt(row.price),
                                 bookingType: 'Custom',
-                                storeId: 1,
+                                storeId: 2,
                                 date: row.date,
                                 paid: true
                             });
